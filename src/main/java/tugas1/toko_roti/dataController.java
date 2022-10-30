e4e84b1110d3487fa16e8328ab1ddd19cc1ab1b5
aulianar <arisky540@gmail.com>
2022-10-30 18:25:39 +0700

    membuat tableController

--- src/main/java/tugas1/toko_roti/dataController.java
+++ src/main/java/tugas1/toko_roti/dataController.java
@@ -11,4 +11,82 @@ package tugas1.toko_roti;
  */
 public class dataController {
     
+    public Double getHarga(String hrgSayur){
+       
+        Double hargaSayur = Double.valueOf(hrgSayur);
+        
+        return hargaSayur;
+    }
+    
+    public Double getJumlah(String jmlSayur){
+    
+        Double jumlahSayur = Double.valueOf(jmlSayur);
+    
+        return jumlahSayur;
+    }
+    
+    public Double gettotalBayar(Double jumlahBayar, Double hargaDiskon){
+    
+        Double totalBayar = jumlahBayar - hargaDiskon;
+        
+        return totalBayar;
+    }
+    
+    public Double getjumlahBayar(Double hrgSayur, Double jmlSayur){
+    
+        Double jumlahBayar = hrgSayur * jmlSayur;
+    
+        return jumlahBayar;
+    }
+    
+    public String getdisc(Double jumlahBayar){
+    
+        String disc = null;
+        
+        if (jumlahBayar <10000) {
+            disc = "10";
+        }else if (jumlahBayar <25000){
+            disc = "10";
+        }else{
+            disc = "15";
+        }
+        return disc;
+    }
+    
+    public Double gethargaDisc(Double jumlahBayar, Integer disc){
+        
+        Double hargaDiskon = jumlahBayar * disc /100;
+    
+        return hargaDiskon;
+    }
+    
+    public void gettdiskon(Double totalBayar, Double jumlahBayar, Double hargaDiskon, Integer diskon){
+        
+        if(jumlahBayar < 16000){
+            diskon = 0;
+            totalBayar = jumlahBayar - (jumlahBayar * diskon / 100);
+            hargaDiskon = jumlahBayar * diskon / 100;
+            
+        }else if(jumlahBayar < 25000){
+            diskon = 10;
+            totalBayar = jumlahBayar - (jumlahBayar * diskon / 100);
+            hargaDiskon = jumlahBayar * diskon / 100;
+        }else {
+            diskon = 15;
+            totalBayar = jumlahBayar - (jumlahBayar * diskon / 100);
+            hargaDiskon = jumlahBayar * diskon / 100;
+        }
+    }
+    
+    public Double getPembayaran(String uangBayar){
+        Double pembayaran = Double.valueOf(uangBayar);
+        
+        return pembayaran;
+    }
+    
+    public Double getKembalian(Double totalBayar, Double uangBayar){
+        Double kembalian = uangBayar - totalBayar;
+        
+        return kembalian;
+    }
 }

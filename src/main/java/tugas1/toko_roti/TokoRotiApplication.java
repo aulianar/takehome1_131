951f6f2be655ae31be315dcc92c7a6d84ed2e02b
aulianar <arisky540@gmail.com>
2022-10-30 16:56:00 +0700

    membuat index

---
+++ src/main/java/tugas1/toko_roti/TokoRotiApplication.java
@@ -0,0 +1,13 @@
+package tugas1.toko_roti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoRotiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoRotiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tugas1/toko_roti/TokoRotiApplicationTests.java
@@ -0,0 +1,13 @@
+package tugas1.toko_roti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoRotiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
